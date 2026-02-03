6f7d552907be8bd56a8b2e4973f4bda86561e95d
Nathan on vivobook <u21551228@tuks.co.za>
2026-02-03 14:43:49 +0200

    messed up klast commit made sure to add the files

--- TestFunctions.py
+++ TestFunctions.py
@@ -1,1 +1,2 @@
-class
+class RequestFromCoconut:
+    def __init__(self):
---
+++ requestFunctions.py
@@ -0,0 +1,13 @@
+import requests 
+
+nameToBaseURLDic ={'Coconut': 'https://coconut.readthedocs.io/en/latest/',}
+
+class RequestFunctions:
+    def __init__(self,TypeofRequest, PlaceToRequestFrom):
+        self.TypeofRequest = TypeofRequest
+        self.PlaceToRequestFrom = PlaceToRequestFrom
+
+    def constructURL(self):
+        pass
+
+
