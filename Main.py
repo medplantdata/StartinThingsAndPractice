d37075434ebddb8b1ea3b604a862a102619dd6c2
Nathan on vivobook <u21551228@tuks.co.za>
2026-02-04 14:38:46 +0200

    will do some debugging now so commiting to save. The code throws an error that apparently cocnut does not return json

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
-print('hello world')
+import streamlit as st
+import streamlit.components.v1 as components
+from TestFunctions import RequestFromCoconut 
+from rdkit import Chem
+
+search_query = st.text_input("Enter your chemical name query:", value = "theobromine")
+
+getMOl = RequestFromCoconut()
+if st.button("Search") and search_query:
+    raw = getMOl.searchMolecule(search_query)
+    st.code(raw[:500])
+
+if st.button("Search"):
+    result = getMOl.searchMolecule(search_query)
+    st.write(result)
